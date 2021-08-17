2a772ec6713e2923e39ca2e87e8f56d5cd1f907e
DellaRovereSandroJunior <dellarovere.sandro@gmail.com>
2021-08-17 15:13:28 +0200

    Correzioni ortografiche

--- getPlantState.js
+++ getPlantState.js
@@ -44,7 +44,7 @@ function getLatestImage (req, res) {
         if(err) throw err;
     });
 
-    var url = req.protocol + '://' + req.get('host') + req.originalUrl + "/latest.jpg";
+    var url = (req.protocol + '://' + req.get('host') + req.originalUrl).slice(0, -1) + "/latest.jpg";
 
     var latestImage= {
         url: url,
